5084f22b8de19e5f5e1dff4bf2052ea2753c1645
Bernard Mugita <mugita68@gmail.com>
2026-03-29 12:41:58 +0300

    feat: minimum UI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About page_name='About'/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-
 export interface Props {
+  page_name: string
 }
 
 const About = (props: Props) => {
   return (
     <div>
-      
+      <h1>{props.page_name}</h1>
     </div>
   );
 }
